e777eb3c7dbda53a8e844a2d509c4321478bda11
billgx <billgx@ksu.edu>
2022-10-15 23:19:05 -0500

    select box determines township displayed

--- tiles/images/findTownship02.js
+++ tiles/images/findTownship02.js
@@ -1,6 +1,18 @@
 // drawing county map image on canvas
 // reference: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
 
+const btn = document.getElementById("btn");
+const sel = document.getElementById("townshipID");
+
+let tIndex = 0;  // index number of the township to display cropped on the map ... changing this changes the image
+
+btn.onclick = (event) => {
+    event.preventDefault();
+    
+    tIndex = sel.selectedIndex;
+    setUpCanvas();
+};
+
 
 function setUpCanvas() {
     const canvas = document.getElementById("canvas");
@@ -8,6 +20,8 @@ function setUpCanvas() {
     
     const img = new Image();
     img.src = "ccmap.png"       //Clay County Township Map
+
+  
 
     const townshipCoords = [
     //  these array data values are for: range, twsp, x, y, width, height 
@@ -18,26 +32,27 @@ function setUpCanvas() {
     //  Only the first two Range values are provided, we'll
     //  need to get the values of Ranges 3 & 4. 
 
-        [1, 6, 57, 123, 222, 222],
-        [1,	7, 57, 343, 222, 222],
-        [1, 8, 55, 564, 222, 222],
-        [1, 9, 53, 785, 222, 222],
-        [1, 10, 51, 1005, 222, 222],
-        [2,	6, 276, 123, 222, 222],    
-        [2,	7, 274, 341, 222, 222],
-        [2,	8, 273, 564, 222, 222],
-        [2,	9, 272, 785, 222, 222],
-        [2,	10, 271, 1006, 222, 222],
- [3, 6, 497, 123, 222, 222],
- [3, 7, 495, 345, 222, 222],
- [3, 8, 494, 567, 222, 222],
- [3, 9, 493, 778, 222, 222],
- [3, 10, 491, 1000, 222, 222],
- [4, 6, 719, 123, 222, 222],
- [4, 7, 717, 345, 222, 222],
- [4, 8, 716, 568, 222, 222],
- [4, 9, 715, 790, 222, 222],
- [4, 10, 714, 1012, 222, 222]
+    [1, 6, 57, 123, 222, 222],
+    [1,	7, 57, 343, 222, 222],
+    [1, 8, 55, 564, 222, 222],
+    [1, 9, 53, 785, 222, 222],
+    [1, 10, 51, 1005, 222, 222],
+    [2,	6, 276, 123, 222, 222],    
+    [2,	7, 274, 341, 222, 222],
+    [2,	8, 273, 564, 222, 222],
+    [2,	9, 272, 785, 222, 222],
+    [2,	10, 271, 1006, 222, 222],
+    [3, 6, 497, 123, 222, 222],
+    [3, 7, 495, 345, 222, 222],
+    [3, 8, 494, 567, 222, 222],
+    [3, 9, 493, 778, 222, 222],
+    [3, 10, 491, 1000, 222, 222],
+    [4, 6, 719, 123, 222, 222],
+    [4, 7, 717, 345, 222, 222],
+    [4, 8, 716, 568, 222, 222],
+    [4, 9, 715, 790, 222, 222],
+    [4, 10, 714, 1012, 222, 222]
+
     ]
 
     let townshipArray = [];         // array to store Township objects
@@ -49,8 +64,6 @@ function setUpCanvas() {
         townshipArray.push(twsp);
         //console.log(twsp);
     }
-
-    let tIndex = 2;  // index number of the township to display cropped on the map ... changing this changes the image
 
     // input image cropping dimensions
     let img_sx = townshipArray[tIndex].x;
@@ -95,16 +108,7 @@ class Township {
 }
 
 
-function loadTownships(){
 
-}
-
-
-
-setUpCanvas();
-
-// create Township objects
-loadTownships();
 
 
 
